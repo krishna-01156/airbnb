5b50000d4ee6b0bed9b1dd3baae9dfabc394de26
Krishna Gupta <krishnajandial771@gmail.com>
2025-06-19 15:11:52 +0530

    wishlist refinement

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -6,13 +6,13 @@ const { isLoggedIn } = require("../middleware");
 // Show all wishlists for the current user
 router.get("/", isLoggedIn, wishlistController.viewWishlists);
 
-// Create a new wishlist
-router.post("/create", isLoggedIn, wishlistController.createWishlist);
+// // Create a new wishlist
+// router.post("/create", isLoggedIn, wishlistController.createWishlist);
 
 // Add a listing to a wishlist
 router.post("/add/:listingId", isLoggedIn, wishlistController.addToWishlist);
 
 // Remove a listing from a wishlist
-router.post("/remove/:listingId", isLoggedIn, wishlistController.removeFromWishlist);
+// router.post("/remove/:listingId", isLoggedIn, wishlistController.removeFromWishlist);
 
 module.exports = router;
