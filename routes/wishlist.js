bc908bf48a4cc7d6abf43761454f682faa78a77c
Krishna Gupta <krishnajandial771@gmail.com>
2025-06-20 13:37:07 +0530

    wishlist setup

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -9,6 +9,10 @@ router.get("/", isLoggedIn, wishlistController.viewWishlists);
 // // Create a new wishlist
 // router.post("/create", isLoggedIn, wishlistController.createWishlist);
 
+// Get all wishlists for the logged-in user (used for modal)
+router.get("/user", isLoggedIn, wishlistController.getUserWishlists);
+
+
 // Add a listing to a wishlist
 router.post("/add/:listingId", isLoggedIn, wishlistController.addToWishlist);
 
