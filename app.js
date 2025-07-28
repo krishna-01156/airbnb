c8f7e5e4a631481fe7114290c8380cb608a639d8
Krishna Gupta <krishnajandial771@gmail.com>
2025-07-28 22:52:07 +0530

    Deployement issues fix

--- app.js
+++ app.js
@@ -103,6 +103,7 @@ app.use((err,req,res,next)=>{
     res.status(statuscode).render("error.ejs",{message});
 })
 
-module.exports = app;
-
+app.listen("8080",()=>{
+    console.log("server is running on port 8080");
+})
 
